0401e289fa7aa925596d9d9f22ac1947fbd92c70
Dan Lockwood <92036309+dlockwood13@users.noreply.github.com>
2026-05-11 15:30:16 +0100

    Add files via upload

---
+++ src/data/reflections.js
@@ -0,0 +1,4 @@
+export const savedReflections = [
+  "I noticed that my anxiety often appears when I treat uncertainty as danger.",
+  "Stoicism feels less like emotional suppression and more like disciplined attention.",
+];
